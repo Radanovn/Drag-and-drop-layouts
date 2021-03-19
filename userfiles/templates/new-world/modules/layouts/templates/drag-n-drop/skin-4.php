de03893ceba3254d4b8e7b609bf8846cb804b495
Nikolay <rachkuw@gmail.com>
2021-03-19 17:16:46 +0200

    Small changes in all skins

--- userfiles/templates/new-world/modules/layouts/templates/drag-n-drop/skin-4.php
+++ userfiles/templates/new-world/modules/layouts/templates/drag-n-drop/skin-4.php
@@ -24,19 +24,19 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
 ?>
 
 
-<section class="section <?php print $layout_classes; ?> edit safe-mode nodrop" field="layout-drag-n-drop-skin-4-<?php print $params['id'] ?>" rel="module">
+<section class="section <?php print $layout_classes; ?> edit safe-mode " field="layout-drag-n-drop-skin-4-<?php print $params['id'] ?>" rel="module">
     <div class="container">
         <div class="row d-flex justify-content-center">
-           <div class="col-md-5 col d-flex">
+           <div class="col-md-6 col d-flex">
                <div class="col-md-6 col">
                    <div class="mb-3"><i class="mdi mdi-earth" style="font-size: 25px;"></i></div>
-                   <h5 class="mb-3 font-weight-bold">Awesome title</h5>
+                   <h6 class="mb-3 font-weight-bold">Awesome title</h6>
                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
                </div>
 
                <div class="col-md-6 col">
                    <div class="mb-3"><i class="mdi mdi-earth" style="font-size: 25px;"></i></div>
-                   <h5 class="mb-3 font-weight-bold">Awesome title</h5>
+                   <h6 class="mb-3 font-weight-bold">Awesome title</h6>
                    <p>Contrary to popular belief, Lorem Ipsum is not simply random text.</p>
                </div>
            </div>
