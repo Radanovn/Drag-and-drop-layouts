881bdde5cc76d44e64b516fd5b48f71efc27e1e0
Nikolay <rachkuw@gmail.com>
2021-03-22 14:25:13 +0200

    changes

--- userfiles/templates/new-world/modules/layouts/templates/drag-n-drop/skin-5.php
+++ userfiles/templates/new-world/modules/layouts/templates/drag-n-drop/skin-5.php
@@ -27,8 +27,8 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
 <section class="section <?php print $layout_classes; ?> edit safe-mode " field="layout-drag-n-drop-skin-5-<?php print $params['id'] ?>" rel="module">
     <div class="container">
         <div class="row d-flex justify-content-center text-center">
-            <div class="col-md-6 col">
-                <img class="rounded-xl pb-4" src="<?php print template_url(); ?>assets/img/image-for-layouts.png" >
+            <div class="col-md-5 col">
+                <img class="rounded-xl pb-4 w-100" src="<?php print template_url(); ?>assets/img/image-for-layouts.png" >
                 <h1 class="mb-3 font-weight-bold">Short Title</h1>
                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <a href="">ut labore et</a> dolore magna aliqua.</p>
             </div>
--- userfiles/templates/new-world/modules/layouts/templates/drag-n-drop/skin-7.php
+++ userfiles/templates/new-world/modules/layouts/templates/drag-n-drop/skin-7.php
@@ -4,7 +4,7 @@
 
 type: layout
 
-name: Video
+name: Image
 
 position: 7
 
@@ -24,11 +24,11 @@ $layout_classes = ' ' . $classes['padding_top'] . ' ' . $classes['padding_bottom
 ?>
 
 
-<section class="section <?php print $layout_classes; ?> edit safe-mode " field="layout-drag-n-drop-skin-7-<?php print $params['id'] ?>" rel="module">
+<section class="section <?php print $layout_classes; ?> edit safe-mode" field="layout-drag-n-drop-skin-7-<?php print $params['id'] ?>" rel="module">
     <div class="container">
         <div class="row d-flex justify-content-center text-center">
-            <div class="col-md-6 col">
-                <img class="rounded-xl pb-4" src="<?php print template_url(); ?>assets/img/video_for_layouts.png" >
+            <div class="col-md-5 col">
+                <img class="rounded-xl pb-4 w-100" src="<?php print template_url(); ?>assets/img/image-for-layouts.png" >
             </div>
         </div>
     </div>
